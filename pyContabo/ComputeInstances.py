7908391b6a8c6ebad1eec15a8ecfb0ffca7413dd
xLeon-python <leon.muscat@outlook.de>
2021-12-31 13:41:06 +0100

    added updateToken() function

--- pyContabo/ComputeInstances.py
+++ pyContabo/ComputeInstances.py
@@ -2,8 +2,11 @@ from . import auth
 from .Instances import Instances
 
 
-class computeMagement:
+class computeInstances:
 
     def __init__(self, client_id, client_secret, api_user, api_password):
         auth.init(client_id, client_secret, api_user, api_password)
         self.Instances = Instances()
+
+    def updateToken(self):
+        auth.token = auth.updateToken()
--- pyContabo/auth.py
+++ pyContabo/auth.py
@@ -5,19 +5,22 @@ from .errors import BadAuth
 
 
 def init(client_id, client_secret, api_user, api_password):
-    client_id = client_id
-    client_secret = client_secret
-    api_user = api_user
-    api_password = api_password
+    global credentials
+    credentials = {
+        "client_id": client_id,
+        "client_secret": client_secret,
+        "api_user": api_user,
+        "api_password": api_password
+    }
     global token
-    token = updateToken(client_id, client_secret, api_user, api_password)
+    token = updateToken()
 
 
-def updateToken(client_id, client_secret, api_user, api_password):
+def updateToken():
     url = "https://auth.contabo.com/auth/realms/contabo/protocol/openid-connect/token"
     headers = CaseInsensitiveDict()
     headers["Content-Type"] = "application/x-www-form-urlencoded"
-    data = f"client_id={client_id}&client_secret={client_secret}&grant_type=password&username={api_user}&password={api_password}"
+    data = f"client_id={credentials['client_id']}&client_secret={credentials['client_secret']}&grant_type=password&username={credentials['api_user']}&password={credentials['api_password']}"
     resp = requests.post(url, headers=headers, data=data)
 
     if int(resp.status_code) == 200:
