200d78a158da7b63bf49ba0a527cd82e4bcf73e8
xLeon-python <leon.muscat@outlook.de>
2021-12-31 12:42:24 +0100

    added Instance reinstall and cancel + some fixes

---
+++ pyContabo/ComputeInstances.py
@@ -0,0 +1,9 @@
+from . import auth
+from .Instances import Instances
+
+
+class computeMagement:
+
+    def __init__(self, client_id, client_secret, api_user, api_password):
+        auth.init(client_id, client_secret, api_user, api_password)
+        self.Instances = Instances()
