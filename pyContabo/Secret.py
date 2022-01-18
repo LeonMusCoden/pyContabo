35a3d5bc5e39fe65b097d8f708c2a3b149167d77
xLeon-python <leon.muscat@outlook.de>
2022-01-18 20:56:29 +0100

    Added Secret

---
+++ pyContabo/Secret.py
@@ -0,0 +1,38 @@
+
+class Secret:
+
+    def __init__(self, json, _http):
+
+        self._http = _http
+
+        self.tenantId = json["tenantId"]
+        self.customerId = json["customerId"]
+        self.secretId = json["secretId"]
+        self.name = json["name"]
+        self.type = json["type"]
+        self.value = json["value"]
+        self.createdAt = json["createdAt"]
+        self.updatedAt = json["updatedAt"]
+
+        self.rawJson = json
+
+    def update(self, name: str, value: str):
+        """updates the name and the value of the secret"""
+
+        resp = self._http.request(type="patch",
+                                  url=f"https://api.contabo.com/v1/secrets/{self.secretId}",
+                                  data={"name": name, "value": value})
+
+        if resp.status_code == 200:
+            return True
+        return False
+
+    def delete(self):
+        """deletes the secret"""
+
+        resp = self._http.request(type="delete",
+                             url=f"https://api.contabo.com/v1/secrets/{self.secretId}")
+
+        if resp.status_code == 204:
+            return True
+        return False
