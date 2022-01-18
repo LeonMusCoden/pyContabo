adf2ddfc29055e4b399af884af92160971ca0cbb
xLeon-python <leon.muscat@outlook.de>
2022-01-18 21:01:39 +0100

    Moved to Contabo.py

--- pyContabo/ComputeInstances.py
+++
@@ -1,9 +0,0 @@
-from .Instances import Instances
-from .http import APIClient
-
-
-class computeInstances:
-
-    def __init__(self, client_id, client_secret, api_user, api_password):
-        self.http = APIClient(client_id, client_secret, api_user, api_password)
-        self.Instances = Instances(self.http)
