ec6840e64d1aff37f7fa9d9e9268e67b37104a3d
xLeon-python <leon.muscat@outlook.de>
2022-01-21 17:11:43 +0100

    fix bad function call

--- pyContabo/audits/InstanceActionsAudits.py
+++ pyContabo/audits/InstanceActionsAudits.py
@@ -3,8 +3,8 @@ from .Audit import InstanceActionsAudit
 
 class InstanceActionsAudits:
 
-    def __init__(self):
-        pass
+    def __init__(self, _http):
+        self._http = _http
 
     def get(self, page=None, pageSize=None, orderBy=None, instanceId=None, requestId=None,
             changedBy=None):
@@ -12,7 +12,7 @@ class InstanceActionsAudits:
 
         url = f"https://api.contabo.com/v1/compute/instances/actions/audits?{f'page={page}&' if page is not None else ''}{f'size={pageSize}&' if pageSize is not None else ''}{f'orderBy={orderBy}&' if orderBy is not None else ''}{f'instanceId={instanceId}&' if instanceId is not None else ''}{f'requestId={requestId}&' if requestId is not None else ''}{f'instanceId={instanceId}&' if instanceId is not None else ''}{f'changedBy={changedBy}&' if changedBy is not None else ''}"
         url = url[:-1]
-        resp = makeRequest(type="get",
+        resp = self._http.request(type="get",
                            url=url)
 
         if resp.status_code == 200:
